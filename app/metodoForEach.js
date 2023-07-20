1f4d78785d42c909dace2b833bab4466f30824cd
Walisson Santos <109308793+WaliSantos@users.noreply.github.com>
2023-07-20 17:28:02 -0300

    Site AluraBooks
    Esse projeto foi importante para aprender mais sobre métodos array

---
+++ app/metodoForEach.js
@@ -0,0 +1,35 @@
+const elementoParaInserirLivros = document.getElementById('livros');
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis');
+
+function exibirOsLivrosNaTela(listaDeLivros){
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = '';
+    elementoParaInserirLivros.innerHTML = ''; 
+    listaDeLivros.forEach(livro =>{
+        let disponivel = verificaDisponibilidade(livro);
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+        <img class= "${disponivel}" src="${livro.imagem}"
+        alt="${livro.alt}" />
+        <h2 class="livro__titulo">
+        ${livro.titulo}
+        </h2>
+        <p class="livro__descricao">${livro.autor}</p>
+        <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+        <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+        </div>
+        </div>
+    `
+    })
+
+       
+}
+
+function verificaDisponibilidade(livro){
+    if (livro.quantidade>0){
+        return 'livro__imagens'
+    }else{
+        return 'livro__imagens indisponivel'
+    }
+}
+
